d2c1b4bc324e1ede9cae7b2d6cb0e1a5e9e47db8
jeppeml <jeppeml@users.noreply.github.com>
2019-09-12 10:26:54 +0200

    Update MyWorld.java

--- Greenfoot/StaticMonkeys/MyWorld.java
+++ Greenfoot/StaticMonkeys/MyWorld.java
@@ -21,10 +21,10 @@ public class MyWorld extends World
         
         for(int x = 0; x<3;x++){
             for(int y = 0; y<3;y++){
-            Monkey monkey = new Monkey((1+x)*y);
-            addObject(monkey, 25+y*75, 75+x*75);
-            monkey.setName(nc.generateName());
-        }
+                Monkey monkey = new Monkey((1+x)*(1+y));
+                addObject(monkey, 25+y*75, 75+x*75);
+                monkey.setName(nc.generateName());
+            }
         }
         /*
         Monkey peterStegger = new Monkey(1);
