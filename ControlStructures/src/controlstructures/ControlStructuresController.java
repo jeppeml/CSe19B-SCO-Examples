f6519e37f07d0886364f45df59a745bda1409cf9
jeppeml <jeppeml@users.noreply.github.com>
2019-10-02 09:41:40 +0200

    .

---
+++ ControlStructures/src/controlstructures/ControlStructuresController.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlstructures;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author jeppjleemoritzled
+ */
+public class ControlStructuresController implements Initializable {
+    
+    
+    @FXML
+    private Button button;
+    @FXML
+    private Label lblOutput;
+    @FXML
+    private TextField txtInput;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        String userInput = txtInput.getText();
+        if(userInput.equals("42"))
+        {
+            lblOutput.setText("The meaning of life, the universe and everything");
+        }
+        
+        if(userInput.equals("Pi"))
+        {
+            lblOutput.setText("" + Math.PI);
+        }
+        /*
+        if(userInput.equals("42"))
+        {
+            lblOutput.setText("The meaning of life, the universe and everything");
+        }*/
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
